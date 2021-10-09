6baf1e5b52f2307b57c340c63e5111ce49ed8fde
leoqyang <leoqyang@tencent.com>
2021-10-09 19:41:43 +0800

    已经检测过的不再检测

--- js/hidden.js
+++ js/hidden.js
@@ -1,9 +1,10 @@
 const hiddenVideo = () => {
-    const itemEles = document.querySelectorAll(".TopstoryItem");
+    const itemEles = document.querySelectorAll(".TopstoryItem:not(.video-check)");
     for (let itemEle of itemEles){
       if (itemEle.querySelector(".ZVideoItem") || itemEle.querySelector(".VideoAnswerPlayer")) {
         itemEle.style.display = 'none';
       }
+      itemEle.className += " video-check";
     }
 }
 
